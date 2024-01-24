1d7367e4e88c8d632b2cc80a6f2e9e0581d99e79
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-24 18:03:54 +0800

    "Second commit, Here I wrote a code where the user can type the phrase or the list of words they want, and also the program will also ask the user for the word they want to count. "

--- exercise7.py
+++ exercise7.py
@@ -2,8 +2,10 @@
 #Exercise 7: Return the count of a given substring from a string
 #Write a program to find how many times substring “Emma” appears in the given string.
 
-string = " caloi claoi caloi caloicaloi caloi caloi eh"
+main_word = input (str("type the words:"))
+count_of_words = input (str("the the word you want to count:"))
+     
+    
+cnt = main_word.count(count_of_words)
 
-cnt = string.count("caloi")
-
-print (cnt)
+print ("The word was used:",cnt,"times")
